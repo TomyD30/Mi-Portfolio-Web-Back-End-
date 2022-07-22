e799e3e523e349ab665fb5d8c6d46785271c7d0e
Tomás Dominguez <tomyd013004@gmail.com>
2022-07-22 17:37:57 -0300

    probanado editar un proyecto 2

---
+++ src/main/java/BackEnd/API/Controller/ProyectoController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BackEnd.API.Controller;
+
+import BackEnd.API.Model.Proyecto;
+import BackEnd.API.Service.ProyectoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author User
+ */
+
+@RestController
+public class ProyectoController {
+    @Autowired
+    ProyectoService proySrv;
+    
+    @PutMapping("/proyectos/guardar")
+    public Proyecto editar(@RequestBody Proyecto proy){
+        proySrv.guardar(proy);
+        return proy;
+    }
+}
