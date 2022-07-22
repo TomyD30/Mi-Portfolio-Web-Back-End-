717ca7c1fb1771afb2d0a540894f494ceb1c8e42
Tomás Dominguez <tomyd013004@gmail.com>
2022-07-22 02:06:01 -0300

    probando heroku

---
+++ script-datos.sql
@@ -0,0 +1,44 @@
+INSERT INTO empresa VALUES
+(1,"./assets/logoUNMDP.png","Universidad Nacional de Mar del Plata");
+
+INSERT INTO experiencia_laboral VALUES
+(1,"Fui ayudante en la materia, dando clases para los estudiantes y preparando examenes","Ayudante en la materia 'Física I'","2do cuatrimestre 2021",1,1),
+(2,"Fui ayudante en la materia en la parte práctica (ayudando a los estudiantes con las guías), y trabajé en el laboratorio de la materia preprando los materiales necesarios y ayudando a los estudiantes a realizar los trabajos","Ayudante en la materia 'Introducción a la Física'","1er cuatrimestre 2022",1,1);
+
+INSERT INTO persona VALUES
+(1,"Dominguez","tomyd013004@gmail.com","./assets/FotoDePerfil.jpg","Tomás","Me llamo Tomás Dominguez, tengo 21 años y estoy cursando #YoProgramo para convertirme en un desarrollador web full stack.Actualmente estoy estudiando Licenciatura en Física, la programacion es algo que tengo como hobbie y me gusta bastante aprender y conocer cosas nuevas de este mundo. Siento que la progamacion es una habilidad útil y sobre todo muy interesante.","Full Stack Developer Jr");
+
+INSERT INTO instituto VALUES
+(1,"./assets/logoUNMDP.png","Universidad Nacional de Mar del Plata"),
+(2,"./assets/logoBalseiro.png","Instituto Balseiro");
+
+INSERT INTO educacion VALUES
+(1,"Licenciatura en Física","2019-2022",1,1),
+(2,"Pasantía de investigación en el tema 'Dinámica y control de procesos catastróficos en redes complejas interdependientes y multicapas'","2021",1,1),
+(3,"Pasantía de investigación en el tema 'Propagación y mitigación de procesos epidémicos y cascada de fallas'","2021",1,1),
+(4,"Licenciatura en Física","Actual",2,1);
+
+INSERT INTO habilidad VALUES
+(1,"60%","Front End",1),
+(2,"25%","Back End",1),
+(3,"37%","adadadda",1),
+(4,"51%","ada",1),
+(5,"5%","ingles xd",1),
+(6,"93%","un capo",1),
+(7,"81%","mmm vos decis",1);
+
+INSERT INTO proyecto VALUES
+(1,"Programa hecho en C que simula una epidemia en una población, y que se puede configurar para ver los efectos de una vacunación.","Simulación epidémica","2021",1),
+(2,"Modelo que representa el crecimiento de...","Modelo de Eden","2021",1),
+(3,"Este portfolio","Mi portfolio Web","En proceso",1);
+
+INSERT INTO link VALUES
+(1,"./assets/github.png","Repositorio","",1),
+(2,"./assets/github.png","Repositorio","",2),
+(3,"./assets/github.png","Repositorio","https://github.com/TomyD30/Mi-Portfolio-Web-Front-End-",3),
+(4,"./assets/web.png","Web","",3);
+
+INSERT INTO red_social VALUES
+(1,"./assets/twitter.png","https://twitter.com/tomydominguez30","twitter",1),
+(2,"./assets/Instagram.png","https://instagram.com/tomydominguezz","instagram",1),
+(3,"./assets/github.png","https://github.com/TomyD30","github",1);
